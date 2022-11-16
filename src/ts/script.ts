a9b3c429526988f9f97bdb09403a63ff57aeffcf
Ayanokoujifl <guleite3@gmail.com>
2022-11-16 11:18:16 -0300

    fifth commit

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,55 +1,58 @@
 export const visibility = () => {
   window.addEventListener('load', () => {
     const div = document.querySelector('#card-video')
-    const calisteniaVideo = document.querySelectorAll('.calistenia-video')
+    const calisteniaVideo =
+      document.querySelectorAll<HTMLElement>('.calistenia-video')
     const calistenia = document.querySelector('.calistenia')
 
     calisteniaVideo.forEach((video) => {
       video.style.display = 'none'
     })
 
-    calistenia.addEventListener('click', () => {
+    calistenia?.addEventListener('click', () => {
       calisteniaVideo.forEach((video) => {
         video.style.display = 'block'
       })
     })
-    calistenia.addEventListener('mouseover', () => {
+    calistenia?.addEventListener('mouseover', () => {
       calisteniaVideo.forEach((video) => {
         video.style.display = 'none'
       })
     })
 
-    const caminhadaVideo = document.querySelectorAll('.caminhada-video')
+    const caminhadaVideo =
+      document.querySelectorAll<HTMLElement>('.caminhada-video')
     const caminhada = document.querySelector('.caminhada')
 
     caminhadaVideo.forEach((video) => {
       video.style.display = 'none'
     })
 
-    caminhada.addEventListener('click', () => {
+    caminhada?.addEventListener('click', () => {
       caminhadaVideo.forEach((video) => {
         video.style.display = 'block'
       })
     })
-    caminhada.addEventListener('mouseover', () => {
+    caminhada?.addEventListener('mouseover', () => {
       caminhadaVideo.forEach((video) => {
         video.style.display = 'none'
       })
     })
 
-    const laboralVideo = document.querySelectorAll('.laboral-video')
+    const laboralVideo =
+      document.querySelectorAll<HTMLElement>('.laboral-video')
     const laboral = document.querySelector('.laboral')
 
     laboralVideo.forEach((video) => {
       video.style.display = 'none'
     })
 
-    laboral.addEventListener('click', () => {
+    laboral?.addEventListener('click', () => {
       laboralVideo.forEach((video) => {
         video.style.display = 'block'
       })
     })
-    laboral.addEventListener('mouseover', () => {
+    laboral?.addEventListener('mouseover', () => {
       laboralVideo.forEach((video) => {
         video.style.display = 'none'
       })
